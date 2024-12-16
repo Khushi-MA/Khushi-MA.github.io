6593ced2206457180da52d2b0bedc6c722136f1e
Khushi-MA <khushi8599@yahoo.com>
2024-12-16 14:57:59 +0530

    home and projects updated. project to refine

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,37 +4,37 @@ import './Projects.css'; // Importing the CSS for styling
 const projects = [
   {
     id: 1,
-    title: 'Analysing Sentiments of Twitter Dataset with DistilBERT and TF-IDF (Python)',
-    image: 'distilbert-project.jpg',
+    title: 'Analysing Sentiments of Twitter Dataset with DistilBERT and TF-IDF',
+    image: 'p (1).png',
     description: `Developed a sentiment analysis model to accurately classify text-based content, achieving 86% accuracy using DistilBERT, outperforming TF-IDF models (Logistic Regression: 82%, SVM: 83%). 
     • Managed data preprocessing and evaluated model performance metrics.`,
   },
   {
     id: 2,
-    title: 'IPL Tickets Using NFT Tokens (Ethereum, Ganache, React)',
-    image: 'nft-marketplace.jpg',
+    title: 'IPL Tickets Using NFT Tokens',
+    image: 'p (2).png',
     description: `Developed a secure, user-friendly NFT marketplace for buying IPL tickets. 
     • Designed the front-end interface with a focus on attractiveness and intuitive user experience. 
     • Documented the project to support the writing of a research paper.`,
   },
   {
     id: 3,
-    title: 'Multilingual Text Classification Using IndicCorp Dataset (Python)',
-    image: 'indiccorp-classification.jpg',
+    title: 'Multilingual Text Classification Using IndicCorp Dataset',
+    image: 'p (3).png',
     description: `To detect languages in a multilingual environment, a Multinomial Naive Bayes classifier is applied to the IndicCorp dataset, achieving 73.37% accuracy in identifying Indian languages like Hindi, Bengali, Tamil, etc.`,
   },
   {
     id: 4,
-    title: 'Food Distribution System (Full Stack Java, SQL, React)',
-    image: 'food-distribution.jpg',
+    title: 'Food Distribution System',
+    image: 'p (4).png',
     description: `Built a website to connect surplus food holders (restaurants and individuals) with NGOs. 
     • Designed and implemented the database schema and developed RESTful APIs using Java for data handling. 
     • Was responsible for designing the user interface with React.`,
   },
   {
     id: 5,
-    title: 'Contact Application (Dockerized Website with Kubernetes)',
-    image: 'contact-application.jpg',
+    title: 'Contact Application',
+    image: 'p (5).png',
     description: `Sole developer for creating, building, deploying, and monitoring a containerized application using Docker and Kubernetes.`,
   },
 ];
